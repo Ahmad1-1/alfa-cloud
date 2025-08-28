242711486878cad5609c63325b2e6f4bfb26f417
Ahmad1-1 <64047756+Ahmad1-1@users.noreply.github.com>
2025-08-28 20:48:24 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "panelku.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "v2ray-vvip"; // Ganti dengan nama workers kalian
+const rootDomain = "alfarizi17062004.workers.dev"; // Ganti dengan domain utama kalian
+const serviceName = "alfa-cloud"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
